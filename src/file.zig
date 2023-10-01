15241f834f7b4316d47b136be3f52417de5f7a97
Josephamumford <josephamumford@gmail.com>
2023-10-01 11:47:51 -0500

    add duplicate arraylist

--- src/file.zig
+++ src/file.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const allocator = std.heap.page_allocator;
+const ArrayList = std.ArrayList;
 
 pub const DirectoryStats = struct { file_count: usize = 0, directory_count: usize = 0 };
 
@@ -13,13 +14,16 @@ pub fn createFile(filename: []const u8) anyerror!void {
 }
 
 pub fn readDirectory(directory: []const u8) anyerror!DirectoryStats {
-    var file_hash_map = std.StringHashMap(u64).init(allocator);
-    std.log.info("Reading directory: {s}", .{directory});
+    var file_hash_map = std.StringHashMap([]const u8).init(allocator);
     var iter_dir = try std.fs.cwd().openIterableDir(directory, .{});
+    var duplicate_files = ArrayList([]const u8).init(allocator);
+
+    std.log.info("Reading directory: {s}", .{directory});
 
     defer {
         iter_dir.close();
         file_hash_map.deinit();
+        duplicate_files.deinit();
     }
 
     var iter = iter_dir.iterate();
@@ -31,11 +35,18 @@ pub fn readDirectory(directory: []const u8) anyerror!DirectoryStats {
             const key = try allocator.alloc(u8, entry.name.len);
             std.mem.copy(u8, key[0..], entry.name);
             const path = try createAbsolutePath(directory, key);
-            const existing_file = try std.fs.openFileAbsolute(path, .{});
-            const file_stat = try existing_file.stat();
 
             //TODO: if entry exists, add to duplicate list
-            try file_hash_map.putNoClobber(key, file_stat.size);
+
+            const hash_result = try file_hash_map.getOrPut(key);
+
+            if (hash_result.found_existing == true) {
+                try duplicate_files.append(path);
+                try duplicate_files.append(hash_result.value_ptr.*);
+            } else {
+                try file_hash_map.put(key, path);
+            }
+
             directory_stats.file_count += 1;
         }
 
@@ -44,9 +55,10 @@ pub fn readDirectory(directory: []const u8) anyerror!DirectoryStats {
         }
     }
 
+    //TODO: Replace with loop through duplicates and write to duplicates found file
     var file_hash_map_iter = file_hash_map.iterator();
     while (file_hash_map_iter.next()) |file_entry| {
-        std.log.info("hash map - {s} : {}", .{ file_entry.key_ptr.*, file_entry.value_ptr.* });
+        std.log.info("hash map - {s} : {s}", .{ file_entry.key_ptr.*, file_entry.value_ptr.* });
     }
 
     return directory_stats;
